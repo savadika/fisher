ad550bd18d062bd5df6f110b968747db44c99b54
backfish <ylf8708@126.com>
2019-02-20 10:46:36 +0800

    新增contextmanager测试

--- test/testcontext.py
+++ test/testcontext.py
@@ -20,13 +20,13 @@ with gogo() as glo:
 
 # ------------使用contextmanager来实现上下文管理器，不太好理解，但是复用性高---------
 
-# 基类
+# 基类,基类中有一个真正使用的方法
 
 class newgogo(object):
     def query(self):
         print('this is new query')
 
-# 上下文管理器
+# 上下文管理器，管理enter和exit
 
 
 @contextmanager
@@ -39,3 +39,17 @@ def my_newgogo():
 # 调用基类的方法
 with my_newgogo() as r:
     r.query()
+
+
+# ----利用contextmanager来解决实际问题-----
+# 打印一本书籍的时候自动加上前后的书名号
+
+@contextmanager
+def book_mark():
+    print("《", end='')
+    yield
+    print("》", end='')
+
+
+with book_mark():
+    print('雪山飞狐', end='')
