feea65e0b8dc3d39450979b9c5b6e24f5f716462
backfish <ylf8708@126.com>
2019-03-09 19:46:12 +0800

    增加fiter_by的改写方法

--- apps/forms/book.py
+++ apps/forms/book.py
@@ -8,3 +8,8 @@ class SearchForms(Form):
     q = StringField(validators=[Length(min=1, max=30)])
     # 如果客户端不传page参数，则默认设置为1
     page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
+
+
+
+
+
--- apps/models/base.py
+++ apps/models/base.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy, BaseQuery
 from sqlalchemy import Column, SmallInteger, String
 from contextlib import contextmanager
 from datetime import datetime
@@ -18,7 +18,20 @@ class SQLAlchemy(_SQLAlchemy):
             raise e
 
 
-db = SQLAlchemy()
+# 高级应用：改写fiter_by函数，实现自己的应用逻辑
+# 1 要搞清楚filter_by的继承关系
+# 2 如何显示基类的改写
+
+class ylfQuery(BaseQuery):
+    def filter_by(self, **kwargs):
+        if 'status' not in kwargs.keys():
+            kwargs['status'] = 1
+        return super(ylfQuery, self).filter_by(**kwargs)
+
+
+# 实例化sqlalchemy的时候自动采用我自己定义的ylfQuery来实现对
+# status的加入
+db = SQLAlchemy(query_class=ylfQuery)
 
 
 class Base(db.Model):
@@ -29,14 +42,14 @@ class Base(db.Model):
     # 记录的创建时间，但是有些时候不一定需要
     create_time = Column(String(200), nullable=False)
 
-    #定义基类的初始化
-    #如果我是在default中定义的，那么每个create_time都是一样的
-    #这个就是类变量和实例变量的区别之一
+    # 定义基类的初始化
+    # 如果我是在default中定义的，那么每个create_time都是一样的
+    # 这个就是类变量和实例变量的区别之一
     def __init__(self):
         self.create_time = String(datetime.now())
 
-
     # 定义一个共有的方法来完成属性的快速赋值
+
     def set_attrs(self, attrs_dict):
         for key, value in attrs_dict.items():
             if hasattr(self, key) and key != 'id':
